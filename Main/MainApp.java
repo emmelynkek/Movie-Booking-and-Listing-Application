8fba6f8f6ead982a3937daba440b33f758c77e4f
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-07 12:46:54 +0800

    Add files via upload

---
+++ Main/MainApp.java
@@ -0,0 +1,122 @@
+package Main;
+import Model.*;
+import Controller.*;
+import Data.*;
+import View.*;
+import java.util.*;
+
+public class MainApp {
+    public static void main(String[] args){
+        Header.printMobalima();
+
+        AdminAccounts adminDb = SerializeDB.readAdminAccounts("Data/AdminAccounts.txt");
+        UserAccounts userDb = SerializeDB.readUserAccounts("Data/UserAccounts.txt");
+        CineplexList cList = SerializeDB.readCineplexList("Data/CineplexList.txt");
+        MovieList mList = SerializeDB.readMovieList("Data/MovieList.txt");
+        mList.checkEndDate();
+        Top5List t5List = SerializeDB.readTop5List("Data/Top5List.txt");
+
+        Scanner sc = new Scanner(System.in);
+        User user = null;
+        Admin admin = null;
+        String id, pwd, input;
+        
+        while(user == null && admin == null){
+            MenuDisplay.printLogin();
+            System.out.print("Enter selection: ");
+            input = sc.nextLine();
+            switch(input){
+                case "1": 
+                    System.out.println("Enter user ID: ");
+                    id = sc.nextLine();
+                    System.out.println("Enter user password: ");
+                    pwd = sc.nextLine();
+                    if (!userDb.isUser(id))
+                        System.out.println("Account not found!");
+                    else if (!userDb.validateUser(id, pwd))
+                        System.out.println("Wrong password!");
+                    else{
+                        System.out.println("Login success!");
+                        user = new User();
+                        user.setUserId(id);
+                    }
+                    break;
+                case "2": 
+                    System.out.println("Enter Admin ID: ");
+                    id = sc.nextLine();
+                    System.out.println("Enter Admin password: ");
+                    pwd = sc.nextLine();
+                    if (!adminDb.isAdmin(id))
+                        System.out.println("Account not found!");
+                    else if (!adminDb.validateAdmin(id, pwd))
+                        System.out.println("Wrong password!");
+                    else{
+                        System.out.println("Login success!");
+                        admin = new Admin();
+                        admin.setAdminId(id);
+                    }
+                    break;
+                default:
+                    System.exit(0);
+            }
+        }
+
+        if (user!=null){
+            MenuDisplay.printUserMenu();
+            System.out.print("Enter selection: ");
+            input = sc.nextLine();
+            while(true){
+                if (input.equals("1"))
+                    MovieDisplayManager.displayMovieList(mList);
+                else if (input.equals("2")){
+                    t5List.sortList(mList);
+                    MovieDisplayManager.displayTop5List(t5List);
+                }
+                else if (input.equals("3"))
+                    MovieDisplayManager.displayMovieDetails(mList);
+                else if (input.equals("4"))
+                    ReviewManager.addReview(user, mList);
+                else if (input.equals("7"))
+                    break;
+                MenuDisplay.printUserMenu();
+                System.out.print("Enter selection: ");
+                input = sc.nextLine(); 
+            }
+        }
+
+        if (admin!=null){
+            MenuDisplay.printAdminMenu();
+            System.out.print("Enter selection: ");
+            input = sc.nextLine();
+            while(true){
+                if (input.equals("1"))
+                    MovieListingManager.createMovieListing(mList);
+                else if (input.equals("2"))
+                    MovieListingManager.updateMovieListing(mList);
+                else if (input.equals("3"))
+                    MovieListingManager.removeMovieListing(mList);
+                else if (input.equals("4"))
+                    ShowTimeManager.createShowTime(cList, mList);
+                else if (input.equals("5"))
+                    ShowTimeManager.updateShowTime(cList, mList);
+                else if (input.equals("6"))
+                    ShowTimeManager.removeShowTime(mList);
+                else if (input.equals("8"))
+                    break;
+                MenuDisplay.printAdminMenu();
+                System.out.print("Enter selection: ");
+                input = sc.nextLine(); 
+            }
+        }
+
+        SerializeDB.writeSerializedObject("Data/AdminAccounts.txt", adminDb);
+        SerializeDB.writeSerializedObject("Data/UserAccounts.txt", userDb);
+        SerializeDB.writeSerializedObject("Data/CineplexList.txt", cList);
+        SerializeDB.writeSerializedObject("Data/MovieList.txt", mList);
+        SerializeDB.writeSerializedObject("Data/Top5List.txt", t5List);
+
+        System.out.printf("\n");
+        System.out.println("Thank you for using MOBALIMA!"); 
+        sc.close();
+    }
+}
