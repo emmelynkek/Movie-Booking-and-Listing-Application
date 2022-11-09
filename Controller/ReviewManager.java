0f7539ee80111e21076ebbf93c4aa89cd0d315c5
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-09 17:28:14 +0800

    Improved exception handling

--- Controller/ReviewManager.java
+++ Controller/ReviewManager.java
@@ -13,17 +13,22 @@ public class ReviewManager {
         System.out.println("Enter review: ");
         String desc = sc.nextLine();
         System.out.println("Enter a rating from 1 to 10: ");
-        int rating = sc.nextInt();
-        input = sc.nextLine();
-        while (rating<1 || rating>10){
-            System.out.println("Error! Rating must be an int from 1 to 10");
-            System.out.println("Enter a rating from 1 to 10: ");
-            rating = sc.nextInt();
+        try{
+            int rating = sc.nextInt();
             input = sc.nextLine();
+            while (rating<1 || rating>10){
+                System.out.println("Error! Rating must be an int from 1 to 10");
+                System.out.println("Enter a rating from 1 to 10: ");
+                rating = sc.nextInt();
+                input = sc.nextLine();
+            }
+            Review review = new Review(user.getId(), desc, rating);
+            cinemaMovie.setReviews(review);
+            System.out.println("Review has been added!");
+            System.out.printf("\n");
         }
-        Review review = new Review(user.getId(), desc, rating);
-        cinemaMovie.setReviews(review);
-        System.out.println("Review has been added!");
-        System.out.printf("\n");
+        catch(InputMismatchException e){
+            System.out.println("Error! Rating must be an int from 1 to 10");
+        }
     }
 }
