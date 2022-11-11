6e1ebb77603e5de3a00953386de343e20f70b019
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 05:39:32 +0800

    Added java doc comments

--- Controller/MovieDisplayManager.java
+++ Controller/MovieDisplayManager.java
@@ -5,7 +5,20 @@ import View.*;
 
 import java.util.*;
 
-public class MovieDisplayManager{  // Contains static methods to display Movie Listings (for Users)
+/**
+  Contains static methods to display Movie Listings for movie-goers.
+  @author Julian Wong
+  @version 1.0
+  @since 2022-11-12
+*/
+public class MovieDisplayManager{
+    
+    /**
+     * Method to display the list of all available cinema movies.
+     * Basic info about each cinema movie is displayed.
+     * Eg: Title, showing status, movie type, overall reviewer rating and overall ticket sales.
+     * @param mList This is the list of all available cinema movies.
+     */
     public static void displayMovieList(MovieList mList){
         List <CinemaMovie> movieList = mList.getList();
         if (movieList.isEmpty()){
@@ -28,7 +41,16 @@ public class MovieDisplayManager{  // Contains static methods to display Movie L
             System.out.printf("\n");
         }
     }
-
+    
+    /**
+     * Method to display the list of top 5 ranking cinema movies.
+     * Top 5 movies are displayed in descending order
+     * Top 5 movies can be ranked by overall reviewer rating or overall ticket sales.
+     * The ranking method is determined by app Admin.
+     * Basic info about each cinema movie is displayed.
+     * Eg: Title, showing status, movie type, overall reviewer rating and overall ticket sales.
+     * @param t5List This is the list of top 5 cinema movies.
+     */
     public static void displayTop5List(Top5List t5List){
         List <CinemaMovie> movieList = t5List.getList();
         if (movieList.isEmpty()){
@@ -59,7 +81,11 @@ public class MovieDisplayManager{  // Contains static methods to display Movie L
             System.out.printf("\n");
         }
     }
-
+    
+    /**
+     * Method to display additional information of a particular cinema movie.
+     * @param mList This is the list of all available cinema movies.
+     */
     public static void displayMovieDetails(MovieList mList){
         Scanner sc = new Scanner(System.in);
         
