e6c76dce41028b3443cffcc6d51ba732bdeb81f3
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 00:35:27 +0800

    Added interface class
    User is now an interface which is implemented by Admin and Moviegoer

--- Model/User.java
+++ Model/User.java
@@ -1,29 +1,9 @@
 package Model;
 import java.io.*;
 
-public class User implements Serializable{
-    private String userId;
-    private String mobileNum;
-    private String email;
+public interface User extends Serializable{
 
-    public User(String id, String num, String email) {
-        this.userId = id;
-        this.mobileNum = num;
-        this.email = email;
-    }
-    public void setUserId(String id){
-        userId = id;
-    }
-
-    public String getId(){
-        return userId;
-    }
-
-    public String getNum() {
-        return this.mobileNum;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
+    public void setId(String id);
+    public String getId();
 }
+
