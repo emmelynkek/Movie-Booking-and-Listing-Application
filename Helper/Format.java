cf3b7a3dc8ad62952592897926cf2b7889fb5802
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 06:22:21 +0800

    Added java doc comments

--- Helper/Format.java
+++ Helper/Format.java
@@ -2,7 +2,19 @@ package Helper;
 
 import java.util.regex.*;
 
+ /**
+  Contains static helper methods to deal with date and time format checking
+  @author Julian Wong
+  @version 1.0
+  @since 2022-11-12
+ */
 public class Format {
+    
+    /**
+     * Method to check if input time string is in Military Time format.
+     * @param input This is the input String to be checked.
+     * @return true if input time string is in Military Time format; return false if otherwise.
+     */
     public static boolean isValidTimeFormat(String input) {
         Pattern pattern = Pattern.compile("[0-2][0-9][0-6][0-9]");
         if(pattern.matcher(input).matches()){
@@ -13,7 +25,12 @@ public class Format {
         System.out.println("Error! Invalid time format entered");
         return false;
     }
-
+    
+    /**
+     * Method to check if input date string is in DD-MMM-YYYY format.
+     * @param input This is the input string to be checked.
+     * @return true if input date string is in in DD-MMM-YYYY format; return false if otherwise.
+     */
     public static boolean isValidDateFormat(String input) {
         Pattern pattern = Pattern.compile("[0-9]{2}-[a-zA-Z]{3}-[0-9]{4}");
         if (pattern.matcher(input).matches())
