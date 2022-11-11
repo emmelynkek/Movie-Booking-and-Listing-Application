8a2fc813468816fb1773456bb25281d1c66760bc
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 04:48:35 +0800

    Added java doc comments

--- Model/MovieList.java
+++ Model/MovieList.java
@@ -4,21 +4,49 @@ import java.io.*;
 import java.time.*;
 import java.time.format.*;
 
+/**
+  Represents the container Class used to store the list of all Cinema Movies in the database.
+  @author Julian Wong
+  @version 1.0
+  @since 2022-11-12
+*/
 public class MovieList implements Serializable{
-    private List <CinemaMovie> movieList;    //contains List of all avaliable movies 
-
+    
+    /**
+     * List of all avaliable cinema movies.
+     */
+    private List <CinemaMovie> movieList;  
+    
+    /**
+     * Creates a MovieList object.
+     */
     public MovieList(){
         movieList = new ArrayList <CinemaMovie>();
     }
-
+    
+    /**
+     * Method to access the list of cinema movies.
+     * @return list of cinema movies.
+     */
     public List <CinemaMovie> getList(){
         return movieList;
     }
-
+    
+    /**
+     * Method to add a cinema movie to the list of cinema movies
+     * @param movie The cinema movie to be added.
+     */
     public void addMovie(CinemaMovie movie){
         movieList.add(movie);
     }
-
+    
+    /**
+     * Method to select a particular cinema movie.
+     * Users can select a cinema movie from the list of all availble cinema movies.
+     * Users will input an integer to make their selection.
+     * @return CinemaMovie object that has been selected by the user.
+     * @retun null if invalid selection is made.
+     */
     public CinemaMovie searchMovie(){
         Scanner sc = new Scanner(System.in);
         int i=1;
@@ -46,8 +74,12 @@ public class MovieList implements Serializable{
             return null;
         }   
     }
-
-    public void checkEndDate(){   // Check endDate of each CinemaMovie and update status to END_OF_SHOWING if endDate == current date.
+    
+    /**
+     * Method to check endDate attribute of each CinemaMovie.
+     * If endDate is equal to the current date, showing status of the cinema movie is updated to END_OF_SHOWING
+     */
+    public void checkEndDate(){ 
         LocalDate date = LocalDate.now();
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
         String formattedDate = date.format(dateFormat);
