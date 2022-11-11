c4384bf17f80ed6c099c1fd4bc4561b8510fdb7f
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 05:31:16 +0800

    Added java doc comments

--- Model/User.java
+++ Model/User.java
@@ -1,9 +1,25 @@
 package Model;
 import java.io.*;
-
+   
+/**
+  Represents the Interface class to store login info for app users.
+  Implemented by Moviegoer class and Admin Class.
+  @author Julian Wong
+  @version 1.0
+  @since 2022-11-12
+*/
 public interface User extends Serializable{
-
+    
+    /**
+     * Method to set the ID for an app user.
+     * @param id This is the ID of an app user.
+     */
     public void setId(String id);
+    
+     /**
+     * Method to access ID of an app user.
+     * @return ID of the an app user.
+     */
     public String getId();
 }
 
