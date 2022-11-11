9c75f4318321ed9b417234c186b158cd56d4e757
Julian <101925327+DarkDestiny26@users.noreply.github.com>
2022-11-12 03:29:37 +0800

    Added java doc comments

--- Model/Admin.java
+++ Model/Admin.java
@@ -1,12 +1,29 @@
 package Model;
 
+/**
+  Represents the Class used to store the ID of app administrators.
+  @author Julian Wong
+  @version 1.0
+  @since 2022-11-12
+*/
 public class Admin implements User{
+    /**
+     * ID of this administrator.
+     */
     private String adminId;
     
+    /**
+     * Method to set or modify the ID for this Admin object.
+     * @param id This is the ID of this administrator.
+     */
     public void setId(String id){
         adminId = id;
     }
-
+    
+    /**
+     * Method to access ID of this administrator.
+     * @return ID of this administrator.
+     */
     public String getId(){
         return adminId;
     }
